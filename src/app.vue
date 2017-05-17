7791eb0aa1ce6a11f30260890119dd5acafdd8b7
saa <saa@mazing.com>
2017-05-17 17:57:05 +0800

    小程序基本框架

---
+++ src/app.vue
@@ -0,0 +1,25 @@
+<script type="text/babel">
+  import vco from 'vco'
+  import {observer, inject} from 'utils/mobx-wechat'
+  const WechatStore = 'system/wechat'
+  const MemberStore = 'wechat/member'
+  @inject(MemberStore, WechatStore)
+  @observer
+  export default class extends vco.app {
+    async onLaunch(options) {
+      await this.store[MemberStore].InitMember()// 会员初始化
+      this.store[WechatStore].InitRoute(options)// 路由初始化
+      this.store[WechatStore].getSystem()
+      this.mazingMember = this.store[MemberStore].member// 约定token
+    }
+  }
+
+</script>
+<style rel="stylesheet/less">
+  @import "less/color";
+  .container {
+    box-sizing: border-box;
+    width: 100%;
+    overflow-x: hidden;
+  }
+</style>
---
+++ src/pages/home/home.vue
@@ -0,0 +1,35 @@
+<template>
+  <view>
+  {{route}}
+  {{system}}
+  </view>
+</template>
+
+<script>
+  import vco from 'vco'
+  import {observer, inject} from 'utils/mobx-wechat'
+  const systemStore = 'system/wechat'
+  var app = getApp()
+  @inject(systemStore)
+  @observer
+  export default class extends vco.page {
+
+    state() {
+      return {
+        route: JSON.stringify(this.store[systemStore].member),
+		system: JSON.stringify(this.store[systemStore].system),
+      }
+    }
+
+
+    async onShow(options) {
+    }
+
+    async onLoad(options) {
+    }
+
+  }
+</script>
+
+<style rel="styltsheet/less">
+</style>
